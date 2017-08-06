4e29547045b39b3e034a90367f5e7ef9e135d19b
Candance Smith <Candance@Candances-MacBook-Air.local>
2017-08-05 22:04:23 -0400

    Added BikeStation Realm object

---
+++ BikeFinder/BikeStation.swift
@@ -0,0 +1,18 @@
+//
+//  BikeStation.swift
+//  BikeFinder
+//
+//  Created by Candance Smith on 8/5/17.
+//  Copyright © 2017 Candance Smith. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class BikeStation: Object {
+    dynamic var stationID = ""
+    dynamic var name = ""
+    dynamic var lat = 0.0
+    dynamic var lon = 0.0
+    dynamic var capacity = 0
+}
